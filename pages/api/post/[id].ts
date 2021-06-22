92386e88ce48a120861ce271b15302a0757b7969
benwizschool <ben@wizschool.io>
2021-06-22 23:50:43 +0900

    Add delete

---
+++ pages/api/post/[id].ts
@@ -0,0 +1,15 @@
+import prisma from "../../../lib/prisma";
+
+export default async function (req, res) {
+  const postId = req.query.id;
+  if (req.method === "DELETE") {
+    const post = await prisma.post.delete({
+      where: { id: Number(postId) }
+    });
+    res.json(post);
+  } else {
+    throw new Error(
+      `The HTTP ${req.method} method is not supported at this route.`
+    );
+  }
+}
