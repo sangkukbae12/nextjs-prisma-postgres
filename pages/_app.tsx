d04f6007051a3e16b51084998b5e343f546bd0c7
benwizschool <ben@wizschool.io>
2021-06-22 23:50:57 +0900

    Add provider

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import { AppProps } from "next/app";
+import { Provider } from "next-auth/client";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Provider session={pageProps.session}>
+      <Component {...pageProps} />
+    </Provider>
+  );
+}
+
+export default MyApp;
